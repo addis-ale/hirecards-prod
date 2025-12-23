ca61de95108086920e0bcb77ec2e5c076f64cfd9
addis-ale <addis.ugr-4143-15@aau.edu.et>
2025-12-23 04:58:50 -0800

    testimonial section

--- src/app/modules/ui/views/home-view.tsx
+++ src/app/modules/ui/views/home-view.tsx
@@ -1,6 +1,7 @@
 import { HomeHeroSection } from "../components/home-hero-section";
 import { HomeNavbar } from "../components/home-nav-bar";
 import { OutPutPlaceholder } from "../components/out-put-placeholder";
+import { Testimonials } from "../components/testimonials";
 
 export const HomeView = () => {
   return (
@@ -8,6 +9,7 @@ export const HomeView = () => {
       <HomeNavbar />
       <HomeHeroSection />
       <OutPutPlaceholder />
+      <Testimonials className="w-full" />
     </div>
   );
 };
